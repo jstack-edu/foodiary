fe1019edc2f05d91c75ae5fa849a81e4dd136b1a
Mateus Silva <mateusilva.dev@gmail.com>
2025-09-06 11:12:17 -0300

    Setup do app

---
+++ foodiaryapp/src/ui/App.tsx
@@ -0,0 +1,27 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Greetings } from './screens/Greetings';
+
+export function App() {
+  const [isFontsLoaded] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Greetings />
+    </SafeAreaProvider>
+  );
+}
